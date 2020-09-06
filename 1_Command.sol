0d178d96c161b231973a4d0ffbb018ac75398520
GGOB2 <rkdaudwh13@naver.com>
2020-09-06 15:53:09 +0900

    구조체, 구조체 배열

--- 1_Command.sol
+++ 1_Command.sol
@@ -1,1 +1,24 @@
 pragma solidity >=0.4.22 < 0.7.0;
+
+contract Cmd {
+    
+    struct Command {
+        
+        string name;
+        uint score;
+        bool sig;
+    }
+
+    Command[] public commands;
+    
+    function setCmd(string memory _cmdName, uint _cmdScore, bool _cmdSig) public {
+        commands.push(Command(_cmdName, _cmdScore, _cmdSig));
+    }
+
+    function getCmd(uint _num) public view returns (string memory getCmdName, uint getCmdScore, bool getCmdSig) {
+        getCmdName = commands[_num].name;
+        getCmdScore = commands[_num].score;
+        getCmdSig = commands[_num].sig;
+    } 
+
+}
--- 2_Verify.sol
+++ 2_Verify.sol
@@ -1,1 +1,2 @@
 pragma solidity >=0.4.22 <0.7.0;
+import "1_Command.sol";
