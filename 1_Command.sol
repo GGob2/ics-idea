ab06e5293193b340760cbdd9ad7d30eb4e5922f1
GGOB2 <rkdaudwh13@naver.com>
2020-09-07 16:26:55 +0900

    전반적인 시스템 기능 /* */

--- 1_Command.sol
+++ 1_Command.sol
@@ -1,17 +1,39 @@
 pragma solidity >=0.4.22 < 0.7.0;
 
-contract Cmd {
+
+
+
+
+/* 
+    1. 중요한 명령과 중요하지 않은 명령으로 구분할 것 
+    (중요한 명령들을 배열에 넣는다고 생각했을 때, 점수를 어떻게 관리할 것인지 생각해야함)
     
+    2. 명령 내리는 과정을 event로 진행할 것인지, 그냥 컨트랙트로만 진행할 지 생각해야함
+     2-1. for문 사용해서 if( 내려진 명령이 중요한 명령 리스트에 있으면 ) --> 검증 컨트랙트 시작 
+
+    3. verify 과정 시작할 때  
+
+  */
+  
+contract Cmd {    
     struct Command {
         
         string name;
         uint score;
-        bool sig;
+        
     }
 
     Command[] public commands;
     
-    function setCmd(string memory _cmdName, uint _cmdScore, bool _cmdSig) public {
+    string[] public names;
+    
+    // significant commands
+    function setName(string memory _cmdd) public {
+        names.push(string('1'));    
+        names.push('2');
+    }
+    
+    function setCmd(string memory _cmdName, uint _cmdScore) public {
         commands.push(Command(_cmdName, _cmdScore, _cmdSig));
     }
 
--- 2_Verify.sol
+++ 2_Verify.sol
@@ -1,2 +1,13 @@
 pragma solidity >=0.4.22 <0.7.0;
+
 import "1_Command.sol";
+
+contract Vefify {
+    
+    Command _cmd;
+
+    constructor() {
+        _cmd = new Command();
+    }
+    
+}
