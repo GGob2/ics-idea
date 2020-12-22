7cbed025b76ae2d4c9b7563323afb761cd0c3215
GGOB2 <rkdaudwh13@naver.com>
2020-12-23 03:39:07 +0900

    chore : refactoring 1_Cmd.sol --> 1_New_Cmd.sol

--- 1_New_Commanc.sol
+++ 1_New_Commanc.sol
@@ -1,17 +1,39 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 < 0.7.0;
 
 contract Command_1 {
+    
+    struct verifying {
+        string verifyingGroupEmpName;
+        uint verifyingGroupEmpScore;
+        uint verifyingGroupEmpTrustScore;
+    }
+    
+    // 명령 3번의 점수
+    uint public cmdScore = 5;
 
+    //  검증 그룹이 검증 해야 할 점수 (명령점수 * 2)
+    uint public doubleOfCmdScore = 10;
+    
+    // 랜덤리스트 생성을 위한 랜덤넘버 리스트
     uint[] public randomNumList = [0,1,2,3,4,5,6,7,8,9];
 
-    constructor() public {
-        issueCmd(3, 5);
+    // 검증 그룹 점수 모으기
+    uint public sumOfVerifyingGroupScore;
+
+    // randomNumList에서 검증 그룹 마지막 번호 저장하기
+    uint public lastOfVerifyingGroup;
+    
+    Emp public _emp = new Emp(); 
+    
+    // 정상적으로 작동
+    uint public empScoreTest = _emp.getEmp(0);
+
+    constructor() public {    
+        issueCmd(3); // 3번 emp가 실행
     }
 
-
-
-
-    function issueCmd(uint _empNum, uint cmdScore) payable public {
+    function issueCmd(uint _empNum) payable public {
         
         // shuffle() - 랜덤리스트 생성
         for (uint256 i = 0; i < randomNumList.length; i++) {
@@ -21,10 +43,23 @@ contract Command_1 {
             randomNumList[i] = temp;
         }
 
+        for(uint i = 0; i < 10; i++) {
+            sumOfVerifyingGroupScore += _emp.getEmp(randomNumList[i]);
+            
+            if(sumOfVerifyingGroupScore >= doubleOfCmdScore) {
+                lastOfVerifyingGroup = i;
+                break;
+            }
+        }        
 
+    }
+    
+    // 검증 그룹이 명령을 검증하는 function --> 검증 그룹의 index를 파라미터로 사용
+    function verify(uint _numOfVerifyingGroup) public payable returns (bool) {
+           
+    }
 
-
-
+    function trustScoreFeedback() public payable {
 
     }
 
@@ -32,6 +67,11 @@ contract Command_1 {
 }
 
 contract Emp {
+
+    constructor() public {
+        setEmp();
+    }
+
      // 직원들의 정보를 담을 구조체
     struct employee {
         uint empNum;
@@ -57,6 +97,23 @@ contract Emp {
         employees.push(employee(employees.length+1, "사장", 9, 8));
 
         return employees.length;
+    }
+    
+    function getEmpScore(uint _empNum) public view returns(uint) {
+        return employees[_empNum].empScore;
+    }
+    
+    function updateEmpTrustScore(uint _empNum, bool _issuedStatus) public {
+        if(_issuedStatus == true) {
+            employees[_empNum].empTrustScore += 1;
+        } else {
+            employees[_empNum].empTrustScore -= 1;
+        }
+    }
+
+
+    function getEmpTrustScore(uint _empNum) public view returns(uint) {
+        return employees[_empNum].empTrustScore;
     }
 }
 
